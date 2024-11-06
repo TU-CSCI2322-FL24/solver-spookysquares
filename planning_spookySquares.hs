f7252ebc9f4101cfd791029330310f873d3b73a9
ghernan5 <144841770+ghernan5@users.noreply.github.com>
2024-11-06 12:14:20 -0600

    adding primary types

--- planning_spookySquares.hs
+++ planning_spookySquares.hs
@@ -1,9 +1,10 @@
-
-
-
 ------------- Planning -------------
 
 type Point = (Int, Int)
 type Edge = (Point, Point)
 type Box = ([Edge], Int)
 type Board = [[Box]]
+
+type Game = Board
+type Move = Edge
+type playerTurn = Bool
