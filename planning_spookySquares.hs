d1141f4baed814d228d9afcb01c9a4128246c2a4
Emma Hennessee <159827456+ehenness@users.noreply.github.com>
2024-11-06 13:00:54 -0600

    Update planning_spookySquares.hs

--- planning_spookySquares.hs
+++ planning_spookySquares.hs
@@ -9,3 +9,15 @@ type Game = Board
 type Move = Edge
 type Player1 = Int
 type Player2 = Int
+
+
+-- Planing 12:30 --
+
+type Game = Board
+type Board = [[Point]]
+type Move = Edge
+
+type Point = (Int, Int)
+type Edge = (Point, Point) -- cause an edge has end points (so the edge is the stuff between the points)
+type Box = [Edge]
+
