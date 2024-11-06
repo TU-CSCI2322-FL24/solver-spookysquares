1055b88063f9192fb98d700e835df48e718f78ec
Emma Hennessee <159827456+ehenness@users.noreply.github.com>
2024-11-06 13:02:31 -0600

    Update planning_spookySquares.hs
    Added shared doc

--- planning_spookySquares.hs
+++ planning_spookySquares.hs
@@ -21,3 +21,4 @@ type Point = (Int, Int)
 type Edge = (Point, Point) -- cause an edge has end points (so the edge is the stuff between the points)
 type Box = [Edge]
 
+-- Shared Planning Doc: https://docs.google.com/document/d/14zdDI9YBC7k1rin2WbVVFhhn5QvC527lzKjWfOn1nJY/edit?tab=t.0
