3a7e79a0ef139a106b086aad33ea85e511d713fa
2023-08-21 <ghernan5@janus13.cs.trinity.edu>
2024-11-08 12:24:17 -0600

    finalizing types

--- spookySquaresSolver.hs
+++ spookySquaresSolver.hs
@@ -1,4 +1,10 @@
 type Point = (Int, Int)
-type Edge = (Point, Point)
-type Box = Point
-type Board = [[Box]]
+type Line = (Point, Direction)
+data Direction = Vertical | Horizontal deriving Eq
+type Box = (Point, Player)
+type Board = [Line]
+
+data Player = PlayerOne | PlayerTwo
+type Game = (Board, Player, [Box])
+type Move = Line
+
