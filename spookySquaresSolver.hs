1ff3d3e597c559e22ef43d43dceac34b8a519a4e
Molly Tyree <mollytyree@Mollys-MacBook-Pro-2.local>
2024-11-10 22:16:17 -0600

    adding calcBox function

--- spookySquaresSolver.hs
+++ spookySquaresSolver.hs
@@ -1,13 +1,29 @@
 type Point = (Int, Int)
-type Line = (Point, Direction)
-data Direction = Vertical | Horizontal deriving Eq
+type Line = (Point, Direction) 
+data Direction = Vertical | Horizontal deriving (Eq, Show)
 type Box = (Point, Player)
 type Board = [Line]
 
-data Player = PlayerOne | PlayerTwo
+data Player = PlayerOne | PlayerTwo deriving Show
 type Game = (Board, Player, [Box])
 type Move = Line
 type Winner = Player
+
+--hard coding values: ((x, y), Direction) for 4x4 grid
+-- (0-3),(0-4) Horizontal
+-- (0-4),(0-3) Vertical
+allLines :: [Line]
+allLines = [((0,0), Horizontal), ((0,1), Horizontal), ((0,2), Horizontal), ((0,3), Horizontal), ((0,4), Horizontal), ((1,0), Horizontal), ((1,1), Horizontal), ((1,2), Horizontal), ((1,3), Horizontal), ((1,4), Horizontal), ((2,0), Horizontal), ((2,1), Horizontal), ((2,2), Horizontal), ((2,3), Horizontal), ((2,4), Horizontal), ((3,0), Horizontal), ((3,1), Horizontal), ((3,2), Horizontal), ((3,3), Horizontal), ((3,4), Horizontal), ((0,0), Vertical), ((1,0), Vertical), ((2,0), Vertical), ((3,0), Vertical), ((4,0), Vertical), ((0,1), Vertical), ((0,2), Vertical), ((0,3), Vertical), ((1,1), Vertical), ((1,2), Vertical), ((1,3), Vertical), ((1,4), Vertical), ((2,1), Vertical), ((2,2), Vertical), ((2,3), Vertical), ((2,4), Vertical), ((3,1), Vertical), ((3,2), Vertical), ((3,3), Vertical), ((3,4), Vertical)]
+
+
+-- to calc all lines in given box. input box of ((0,0), Player1)
+calcBox :: Box -> [Line]
+calcBox box = 
+    let point = fst box
+        horPoint = (fst point + 1, snd point)
+        vertPoint = (fst point, snd point + 1)
+    in [boxLines | boxLines <- allLines, (point, Horizontal) == boxLines || (point, Vertical) == boxLines || (horPoint, Horizontal) == boxLines || (vertPoint, Vertical) == boxLines]
+
 
 
 -- Story 2 : Determine who has won the game. Write a function Game -> Winner
