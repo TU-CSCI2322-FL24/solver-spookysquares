3759c1819e4b05e6318d5b5986c431a304823b13
Gael Hernandez <ghernan5@trinity.edu>
2024-11-10 20:15:06 -0600

    setting up comments to separate code

--- spookySquaresSolver.hs
+++ spookySquaresSolver.hs
@@ -8,3 +8,23 @@ data Player = PlayerOne | PlayerTwo
 type Game = (Board, Player, [Box])
 type Move = Line
 
+
+-- Story 2 : Determine who has won the game. Write a function Game -> Winner
+
+
+
+-- Story 3 : Compute the result of making a legal move in a game state, write a function of type
+-- Game -> Move -> Game
+
+
+
+-- Story 4 : Compute the legal moves from a game state, use a function Game -> [Move]
+
+
+
+-- Story 5 : Pretty-print a game into a string, create a function Game -> String
+-- you should NOT override the "Show" typeclass. 
+
+
+
+-- Story 6 : All functions should consider possible errors or edge cases
