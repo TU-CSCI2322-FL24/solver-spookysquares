92217d76494fce5b64af9ff87bbbe3f279f2810b
Gael Hernandez <ghernan5@trinity.edu>
2024-11-10 20:22:31 -0600

    Updating comments

--- spookySquaresSolver.hs
+++ spookySquaresSolver.hs
@@ -10,21 +10,21 @@ type Move = Line
 
 
 -- Story 2 : Determine who has won the game. Write a function Game -> Winner
-
+-- Ana, Adrian
 
 
 -- Story 3 : Compute the result of making a legal move in a game state, write a function of type
 -- Game -> Move -> Game
-
+-- Emma
 
 
 -- Story 4 : Compute the legal moves from a game state, use a function Game -> [Move]
-
+-- Gael, Molly
 
 
 -- Story 5 : Pretty-print a game into a string, create a function Game -> String
 -- you should NOT override the "Show" typeclass. 
-
+-- Aidan
 
 
 -- Story 6 : All functions should consider possible errors or edge cases
